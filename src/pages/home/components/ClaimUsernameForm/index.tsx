e59804de2df759698bded3b0e482bc6dab4f0cae
luismda <luismigueldutraalves@gmail.com>
2023-04-21 08:15:07 -0300

    feat: create form validation in homepage

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,15 +1,58 @@
-import { Button, TextInput } from '@ig-ui/react'
+import { Button, Text, TextInput } from '@ig-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { Form } from './style'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import { Form, FormAnnotation } from './style'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^[a-z]([a-z-]+)[a-z]$/i, {
+      message: 'O usuário deve ter apenas letras e hifens para separação.',
+    })
+    .toLowerCase(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput variant="sm" prefix="call.com/" placeholder="seu-usuario" />
-      <Button size="sm" type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          variant="sm"
+          prefix="call.com/"
+          placeholder="seu-usuario"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome de usuário desejado.'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
