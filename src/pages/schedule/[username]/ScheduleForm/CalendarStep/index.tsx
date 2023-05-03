33e103f3df30170dde930dbe742d6fe8b4d99012
luismda <luismigueldutraalves@gmail.com>
2023-05-03 11:14:41 -0300

    fix: blocked unavailable times by client side

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -15,7 +15,7 @@ import {
 
 interface Availability {
   possibleTimes: number[]
-  availableTimes: number[]
+  unavailableTimes: Date[]
 }
 
 interface CalendarStepProps {
@@ -65,6 +65,18 @@ export function CalendarStep({ onSelectDateTime }: CalendarStepProps) {
     },
   )
 
+  const unavailableTimes = availability?.unavailableTimes.map(
+    (unavailableTime) => {
+      const hours = dayjs(unavailableTime).get('hour')
+      const minutes = dayjs(unavailableTime).get('minute')
+
+      const unavailableTimeInMinutes = hours * 60 + minutes
+      const unavailableTimeInHours = unavailableTimeInMinutes / 60
+
+      return unavailableTimeInHours
+    },
+  )
+
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar selectedDate={selectedDate} onSelectedDate={setSelectedDate} />
@@ -76,26 +88,30 @@ export function CalendarStep({ onSelectDateTime }: CalendarStepProps) {
           </TimePickerHeader>
 
           <TimePickerList>
-            {availability?.possibleTimes.map((hour) => {
-              const timeInMinutes = hour * 60
-              const minutes = timeInMinutes % 60
+            {availability &&
+              availability.possibleTimes.map((hour) => {
+                const timeInMinutes = hour * 60
+                const minutes = timeInMinutes % 60
 
-              return (
-                <TimePickerItem
-                  key={hour}
-                  onClick={() => handleSelectTime(hour)}
-                  disabled={
-                    !availability.availableTimes.includes(hour) ||
-                    dayjs(selectedDate).set('hour', hour).isBefore(new Date())
-                  }
-                >
-                  {dayjs(selectedDate)
-                    .set('hour', hour)
-                    .set('minutes', minutes)
-                    .format('HH:mm[h]')}
-                </TimePickerItem>
-              )
-            })}
+                return (
+                  <TimePickerItem
+                    key={hour}
+                    onClick={() => handleSelectTime(hour)}
+                    disabled={
+                      unavailableTimes?.includes(hour) ||
+                      dayjs(selectedDate)
+                        .set('hour', hour)
+                        .set('minutes', minutes)
+                        .isBefore(new Date())
+                    }
+                  >
+                    {dayjs(selectedDate)
+                      .set('hour', hour)
+                      .set('minutes', minutes)
+                      .format('HH:mm[h]')}
+                  </TimePickerItem>
+                )
+              })}
           </TimePickerList>
         </TimePicker>
       )}
