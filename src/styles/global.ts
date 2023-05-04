974f313efd5cc2f3610f836b2cc20e777a69ea6d
luismda <luismigueldutraalves@gmail.com>
2023-05-04 17:49:17 -0300

    feat: create custom scrollbar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,5 +11,19 @@ export const globalStyles = globalCss({
     backgroundColor: '$gray900',
     color: '$gray100',
     '-webkit-font-smoothing': 'antialiased',
+
+    '&::-webkit-scrollbar': {
+      width: '$2',
+    },
+
+    '&::-webkit-scrollbar-thumb': {
+      background: '$gray500',
+      borderRadius: '$md',
+    },
+
+    '&::-webkit-scrollbar-track': {
+      background: '$gray600',
+      borderRadius: '$md',
+    },
   },
 })
